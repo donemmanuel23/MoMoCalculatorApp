642a1c3994bfa906ffff8e025709b7e00f5c0c88
donemmanuel23 <emmanuelademun23@gmail.com>
2026-04-06 04:47:53 +0300

    initial commit

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -11,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.ndejje.momocalc.ui.theme.MoMoCalculatorAppTheme
 
@@ -52,4 +55,26 @@ fun InternalStateInput (){
   )
 }
 
-
+@Composable
+fun HoistedAmountInput(
+  amount: String,
+  onAmountChange: (String) -> Unit,
+  modifier: Modifier = Modifier,
+  isError: Boolean = false
+) {
+  Column(modifier = modifier) {
+    TextField(
+      value = amount,
+      onValueChange = onAmountChange,
+      isError = isError,
+      label = { Text(stringResource(R.string.enter_amount)) }
+    )
+    if (isError) {
+      Text(
+        text = stringResource(R.string.error_numbers_only),
+        color = MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.bodySmall
+      )
+    }
+  }
+}
