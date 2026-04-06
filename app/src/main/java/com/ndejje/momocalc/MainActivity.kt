d7d644dd684cd0d057f4b00ba3293859b8bb7a9b
donemmanuel23 <emmanuelademun23@gmail.com>
2026-04-06 04:42:14 +0300

    initial commit

---
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.ndejje.momocalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.ndejje.momocalc.ui.theme.MoMoCalculatorAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      MoMoCalculatorAppTheme {
+
+        }
+      }
+    }
+  }
+
+
+@Composable
+fun BrokenInput (){
+var amount = "0"
+
+  TextField(
+    value = amount,
+    onValueChange = { amount = it },
+    label = {
+      Text(stringResource(R.string.enter_amount))
+    },
+  )
+}
+
+@Composable
+fun InternalStateInput (){
+  var amount by remember { mutableStateOf("50000") }
+  TextField(
+    value = amount,
+    onValueChange = { amount = it },
+    label = {
+      Text(stringResource(R.string.enter_amount))
+    },
+  )
+}
+
+
